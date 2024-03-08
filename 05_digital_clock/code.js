8ac8ecb412a3626d0121b214a37a987b4a655558
Krishna Chaurasia <krishnachaurasia045@gmail.com>
2024-03-08 19:53:43 +0530

    project

---
+++ 05_digital_clock/code.js
@@ -0,0 +1,10 @@
+//const clock = document.querySelector(#clock);
+const clock = document.getElementById('clock');
+
+
+//setInterval() : has two parameters(function(){}, time in ms)
+setInterval(function(){
+    let date = new Date();
+   // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString();
+},1000) //it tells when to reload/rerun again and again after specific time
